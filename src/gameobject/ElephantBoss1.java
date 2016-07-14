8dc2e6b3ff315cc8aa317b7cf5ad1fda71f9e25a
PhanHuy <phandinhhuy95@gmail.com>
2016-07-14 19:03:54 +0700

    .

---
+++ src/gameobject/ElephantBoss1.java
@@ -0,0 +1,13 @@
+package gameobject;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by hoanhan on 7/13/16.
+ */
+public class ElephantBoss1 extends Elephant{
+
+    public ElephantBoss1(int posX, int type, int speed) {
+        super(posX, type, speed);
+    }
+}
---
+++ src/gameobject/ElephantBrown.java
@@ -0,0 +1,10 @@
+package gameobject;
+
+/**
+ * Created by HP on 7/14/2016.
+ */
+public class ElephantBrown extends Elephant {
+    public ElephantBrown(int posX, int type, int speed) {
+        super(posX, type, speed);
+    }
+}
